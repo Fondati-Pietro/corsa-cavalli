9b929ff6e1ccb86627ad8b3aa00b5df5c970fcb2
lai2-24 <lai2-24@itismeucci.com>
2024-10-04 10:42:17 +0200

    secondo commit

--- src/main/java/com/example/Cavalli.java
+++ src/main/java/com/example/Cavalli.java
@@ -1,5 +1,26 @@
 package com.example;
 
-public class Cavalli {
+import java.util.ArrayList;
+
+public class Cavalli extends Thread{
+    int meta;
+    ArrayList<String> cavalli;
+
+    public Cavalli(ArrayList<String> c) {
+        meta = 50;
+        this.cavalli = c;
+    }
+
+    public void run(){
+        for (int i = 0; i < meta; i++) {
+            try {
+                Thread.sleep((int) (Math.random() * 1000));
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+        cavalli.add(this.getName());
+    }
+    
     
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,26 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Inizio");
-        
+
+        ArrayList<Cavalli> cavallo = new ArrayList<Cavalli>();
+        ArrayList<String> nome = new ArrayList<String>();
+
+        for (int i = 0; i < 20; i++) {
+            Cavalli cav = new Cavalli(nome);
+            cavallo.add(cav);
+        }
+        for (int i = 0; i < cavallo.size(); i++) {
+            cavallo.get(i).start();
+        }
+        for (int i = 0; i < cavallo.size(); i++) {
+            cavallo.get(i).join();
+        }
+        for (int i = 1; i < cavallo.size(); i++) {
+            System.out.println(i + ": " + nome.get(i));
+        }
     }
 }
